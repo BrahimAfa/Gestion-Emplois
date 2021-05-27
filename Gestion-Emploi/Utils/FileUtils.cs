1c3e7dbec04e8b724071dccbda1570f62ff206a9
Brahim <brahimafassy@gmail.com>
2021-05-27 17:16:47 +0100

    fixed httpContext null reference

--- Gestion-Emploi/Utils/FileUtils.cs
+++ Gestion-Emploi/Utils/FileUtils.cs
@@ -11,7 +11,16 @@ namespace GestionEmploi.Utils
   {
     public static Config LoadJson()
     {
-      string file = HttpContext.Current.Server.MapPath("~/Utils/GestionEmploisConfig.json");
+      string file;
+      if(HttpContext.Current is null)
+      {
+        file = Directory.GetParent(Directory.GetCurrentDirectory()) + @"\Utils\GestionEmploisConfig.json";
+
+      }
+      else
+      {
+        file = HttpContext.Current.Server.MapPath("~/Utils/GestionEmploisConfig.json");
+      }
       using (StreamReader r = new StreamReader(file))
       {
         string json = r.ReadToEnd();
